e4bbf0aa8ab225933e70b77e254748e1f9096e5a
Théo Plénet <theo.plenet@gmail.com>
2025-03-11 14:36:32 +0100

    docs: add docstring to package.

--- test_package/simple_math.py
+++ test_package/simple_math.py
@@ -2,18 +2,22 @@ from math import sqrt
 
 
 def add(x: int, y: int) -> int:
+    """Add 2 integers."""
     return x + y
 
 
 def sub(x: int, y: int) -> int:
+    """Subtract 2 integers."""
     return x - y
 
 
 def mul(x: int, y: int) -> int:
+    """Multiply 2 integers."""
     return x * y
 
 
 def div(x: int, y: int) -> int:
+    """Divide 2 integers and return the integer division."""
     if y == 0:
         raise ValueError("Cannot divide by 0.")
 
@@ -21,6 +25,7 @@ def div(x: int, y: int) -> int:
 
 
 def div_f(x: int, y: int) -> float:
+    """Divide 2 integers and return the float division."""
     if y == 0:
         raise ValueError("Cannot divide by 0.")
 
@@ -28,8 +33,10 @@ def div_f(x: int, y: int) -> float:
 
 
 def root(x: int) -> float:
+    """Compute the square root of an integer."""
     return sqrt(x)
 
 
 def square(x: int) -> int:
+    """Compute the square of an integer."""
     return x * x
